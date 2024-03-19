47d0ca87a6cdfe26cd2d391ca88711ac4dd5ba70
Connor Cowgill <107578039+conno1771@users.noreply.github.com>
2024-03-19 19:12:22 -0400

    Add files via upload

---
+++ dating-app-api/dating-app-api/dating-app-api/Helpers/UserSkillHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace dating_app_api.Helpers
+{
+    public class UserSkillHelper
+    {
+        public int? UserId { get; set; } = 0;
+        public string? Skill { get; set; } = "";
+        public string? Token { get; set; } = "";
+
+        public UserSkillHelper(int userid, string skill, string token)
+        {
+            UserId = userid;
+            Skill = skill;
+            Token = token;
+        }
+    }
+}
