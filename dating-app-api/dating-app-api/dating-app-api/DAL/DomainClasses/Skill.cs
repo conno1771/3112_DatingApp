f1a2fdd7ed31a832d69ba8f5c335dfd121f7aa39
Connor Cowgill <107578039+conno1771@users.noreply.github.com>
2024-03-19 19:11:46 -0400

    Added UserSkill and Skill Domain Classes
    Domain Classes to provide objects that correspond to the database

---
+++ dating-app-api/dating-app-api/dating-app-api/DAL/DomainClasses/Skill.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace dating_app_api.DAL.DomainClasses
+{
+    public class Skill
+    {
+        [Key]
+        public string SkillName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ dating-app-api/dating-app-api/dating-app-api/DAL/DomainClasses/UserSkill.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dating_app_api.DAL.DomainClasses
+{
+    [PrimaryKey("UserID", "Skill")]
+    public class UserSkill
+    { public int UserID { get; set; }
+        public string Skill{ get; set; }
+
+        public UserSkill(int UserID, string Skill)
+        {
+            this.UserID = UserID;
+            this.Skill = Skill;
+        }
+    }
+}
