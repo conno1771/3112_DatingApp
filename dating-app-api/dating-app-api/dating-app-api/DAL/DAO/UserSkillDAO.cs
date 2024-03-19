91b24fef6aae5043aae92dc6eb9ab3d0b3f2e90f
Connor Cowgill <107578039+conno1771@users.noreply.github.com>
2024-03-19 19:10:21 -0400

    Add files via upload

---
+++ dating-app-api/dating-app-api/dating-app-api/DAL/DAO/UserSkillDAO.cs
@@ -0,0 +1,36 @@
+﻿using dating_app_api.DAL.DomainClasses;
+
+namespace dating_app_api.DAL.DAO
+{
+    public class UserSkillDAO
+    {
+        private readonly AppDbContext _db;
+        public UserSkillDAO(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<UserSkill> AddUserSkill(int uid, string skill)
+        {
+            UserSkill? userSkill = new UserSkill(uid, skill); 
+            //using try catch because skill must be one of those in the skill table
+            //should only throw exception during testing
+            try
+            {
+
+                await _db.UserSkills!.AddAsync(userSkill);
+                await _db.SaveChangesAsync();
+                return userSkill;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public async Task<UserSkill> GetUserSkill(int uid, string skill)
+        {
+            UserSkill? userSkill = await _db.UserSkills!.FindAsync(uid, skill);
+            return userSkill;
+        }
+    }
+}
