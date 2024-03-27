6e6565c2e39de4373f3638b060a576066590b45e
Connor Cowgill <107578039+conno1771@users.noreply.github.com>
2024-03-26 20:06:16 -0400

    Update UserSkillHelper.cs
    Removed constructor, using UserSkillHelper() { fields here }

--- dating-app-api/dating-app-api/dating-app-api/Helpers/UserSkillHelper.cs
+++ dating-app-api/dating-app-api/dating-app-api/Helpers/UserSkillHelper.cs
@@ -5,12 +5,5 @@
         public int? UserId { get; set; } = 0;
         public string? Skill { get; set; } = "";
         public string? Token { get; set; } = "";
-
-        public UserSkillHelper(int userid, string skill, string token)
-        {
-            UserId = userid;
-            Skill = skill;
-            Token = token;
-        }
     }
 }
