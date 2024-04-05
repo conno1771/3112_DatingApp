976a4a0a3d9eb90b1cfd164ebaac1916e8027c4e
Connor Cowgill <107578039+conno1771@users.noreply.github.com>
2024-04-05 19:10:57 -0400

    Create ReviewHelper.cs
    added review helper class

---
+++ dating-app-api/dating-app-api/dating-app-api/Helpers/ReviewHelper.cs
@@ -0,0 +1,11 @@
+namespace dating_app_api.Helpers
+{
+    public class ReviewHelper
+    {
+        public int? Id { get; set; } = 0;
+        public int? Quality { get; set; } = 0;
+        public int? UserId { get; set; } = 0;
+        public string? ReviewDate { get; set; } = null;
+        public string? Token { get; set; } = "";
+    }
+}
